763b87e895addd98d37e326c9863f722f2180bd6
Margaux Letellier <lmargauxgo@aol.com>
2020-04-10 22:17:45 +0200

    first commit
    first commit

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -1,94 +1,208 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router } from 'react-router-dom';
-import { MDBNavbar, 
-  MDBNavbarBrand, 
-  MDBNavbarNav, 
-  MDBNavItem, 
-  MDBNavLink, 
-  MDBNavbarToggler, 
-  MDBCollapse, 
-  MDBDropdown,
-  MDBDropdownToggle, 
-  MDBDropdownMenu, 
-  MDBDropdownItem, 
-  MDBIcon } from "mdbreact";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import {
+  MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler,
+  MDBCollapse,
+  MDBMask,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBBtn,
+  MDBView,
+  MDBContainer,
+  MDBCard, MDBCardBody, MDBCardText,
+  MDBInput,
+  MDBAnimation,
+  MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, 
+} from "mdbreact";
 
-import "./navBar.css"
+import "./navBar.css";
 import picture from "../../pictures/IMG_6767.JPG";
 
-class Navbar extends Component {
-state = {
-  isOpen: false
-};
+class ClassicFormPage extends React.Component {
+  state = {
+    collapseID: ""
+  };
 
-toggleCollapse = () => {
-  this.setState({ isOpen: !this.state.isOpen });
-}
+  toggleCollapse = collapseID => () =>
+    this.setState(prevState => ({
+      collapseID: prevState.collapseID !== collapseID ? collapseID : ""
+    }));
 
-render() {
-  return (
+  render() {
 
-      <Router>
+    const overlay = (
+      <div
+        id="sidenav-overlay"
+        style={{ backgroundColor: "transparent" }}
+        onClick={this.toggleCollapse("navbarCollapse")}
+      />
+    );
 
-        <MDBNavbar color="blue-grey" dark expand="md">
+    return (
+      <div id="classicformpage">
 
-          <MDBNavbarBrand>
-            <img alt="Ma photo" id="pic" src={picture} height="100" />
-            <strong id="haut" className="white-text">MonCV</strong>
-          </MDBNavbarBrand>
+        <Router>
 
-          <MDBNavbarToggler onClick={this.toggleCollapse} />
+          <div>
+            <MDBNavbar dark expand="md" fixed="top">
 
-          <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+              <MDBContainer>
 
-            <MDBNavbarNav left>
-              <MDBNavItem active>
-                <MDBNavLink to="#!">Contact</MDBNavLink>
-              </MDBNavItem>
-              <MDBNavItem>
-                <MDBNavLink to="#!">Compétences</MDBNavLink>
-              </MDBNavItem>
-              <MDBNavItem>
-                <MDBNavLink to="#!">Expérience</MDBNavLink>
-              </MDBNavItem>
-              <MDBNavItem>
-                <MDBNavLink to="#!">Formation</MDBNavLink>
-              </MDBNavItem>
-            </MDBNavbarNav>
+                <MDBNavbarBrand>
+                  <img alt="Ma photo" id="pic" src={picture} />
+                  <strong id="haut" className="white-text">MonCV</strong>
+                </MDBNavbarBrand>
 
-            <MDBNavbarNav right>
-              <MDBNavItem>
-                <MDBNavLink className="waves-effect waves-light" to="#!">
-                  <MDBIcon fab icon="twitter" />
-                </MDBNavLink>
-              </MDBNavItem>
-              <MDBNavItem>
-                <MDBNavLink className="waves-effect waves-light" to="#!">
-                  <MDBIcon fab icon="google-plus-g" />
-                </MDBNavLink>
-              </MDBNavItem>
-              <MDBNavItem>
-                <MDBDropdown>
-                  <MDBDropdownToggle nav caret>
-                    <MDBIcon icon="user" />
-                  </MDBDropdownToggle>
-                  <MDBDropdownMenu className="dropdown-default">
-                    <MDBDropdownItem href="#!">Action</MDBDropdownItem>
-                    <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
-                    <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-                    <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-                  </MDBDropdownMenu>
-                </MDBDropdown>
-              </MDBNavItem>
-            </MDBNavbarNav>
+                <MDBNavbarToggler
+                  onClick={this.toggleCollapse("navbarCollapse")}
+                />
+                <MDBCollapse
+                  id="navbarCollapse"
+                  isOpen={this.state.collapseID}
+                  navbar
+                >
+                <MDBNavbarNav left>
+                  <MDBNavItem active>
+                    <MDBNavLink to="#!">Contact</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Compétences</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Expérience</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Formation</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
 
-          </MDBCollapse>
-        </MDBNavbar>
+                <MDBNavbarNav right>
+                  <MDBNavItem>
+                    <MDBNavLink className="waves-effect waves-light" to="#!">
+                      <MDBIcon fab icon="twitter" />
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink className="waves-effect waves-light" to="#!">
+                      <MDBIcon fab icon="google-plus-g" />
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBDropdown>
+                      <MDBDropdownToggle nav caret>
+                        <MDBIcon icon="user" />
+                      </MDBDropdownToggle>
+                      <MDBDropdownMenu className="dropdown-default">
+                        <MDBDropdownItem href="#!">Action</MDBDropdownItem>
+                        <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
+                        <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                        <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                      </MDBDropdownMenu>
+                    </MDBDropdown>
+                  </MDBNavItem>
+                </MDBNavbarNav>
 
-      </Router>
+                </MDBCollapse>
 
+              </MDBContainer>
+
+            </MDBNavbar>
+            {this.state.collapseID && overlay}
+          </div>
+
+        </Router>
+
+        <MDBView>
+          <MDBMask className="d-flex justify-content-center align-items-center gradient">
+            <MDBContainer>
+              <MDBRow>
+                <MDBAnimation
+                  type="fadeInLeft"
+                  delay=".3s"
+                  className="white-text text-center text-md-left col-md-6 mt-xl-5 mb-5"
+                >
+                  <h1 className="h1-responsive font-weight-bold">
+                  Développeuse Web junior
+                  </h1>
+                  <hr className="hr-light" />
+                  <h6 className="mb-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Rem repellendus quasi fuga nesciunt dolorum nulla magnam
+                    veniam sapiente, fugiat! Commodi sequi non animi ea dolor
+                    molestiae, quisquam iste, maiores. Nulla.
+                  </h6>
+                </MDBAnimation>
+
+                <MDBCol md="6" xl="5" className="mb-4">
+                  <MDBAnimation type="fadeInRight" delay=".3s">
+                    <MDBCard id="classic-card">
+                      <MDBCardBody className="white-text">
+
+                        <h3 className="text-center">
+                          <MDBIcon icon="user" /> Margaux Letellier
+                        </h3>
+                        <hr className="hr-light" />
+
+                        <MDBCardText className="white-text">
+                        Sed ut perspiciatis unde omnis iste natus sit voluptatem
+                        accusantium doloremque laudantium, totam rem aperiam.{' '}
+                        </MDBCardText>
+
+                        <div className="text-center mt-4 black-text">
+                          <hr className="hr-light" />
+                          <div className="text-center d-flex justify-content-center white-label">
+                            <a href="https://www.linkedin.com/in/margaux-letellier-a0447517b/" className="p-2 m-2">
+                              <MDBIcon
+                                fab
+                                icon="linkedin"
+                                className="white-text"
+                              />
+                            </a>
+                            <a href="https://github.com/MargauxLet" className="p-2 m-2">
+                              <MDBIcon
+                                fab
+                                icon="github"
+                                className="white-text"
+                              />
+                            </a>
+                            <a href="https://gitlab.com/MargauxLet" className="p-2 m-2">
+                              <MDBIcon
+                                fab
+                                icon="gitlab"
+                                className="white-text"
+                              />
+                            </a>
+                          </div>
+                        </div>
+
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBAnimation>
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+          </MDBMask>
+        </MDBView>
+
+        <MDBContainer>
+          <MDBRow className="py-5">
+            <MDBCol md="12" className="text-center">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </div>
     );
   }
 }
 
-export default Navbar;
+export default ClassicFormPage;
