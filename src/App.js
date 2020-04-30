94a47ef4893923b5f936acd30a35be2a20e4df43
Margaux Letellier <lmargauxgo@aol.com>
2020-04-30 20:39:59 +0200

    commit 5.2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./index.css";
 import { BrowserRouter as Router} from "react-router-dom";
-import Routes from "./components/routes";
+import Routes from "../src/components/routes";
 
 function App() {
     return (
